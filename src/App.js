df62f95c93823f7a9b5681d87592974011bf15e6
Abhishek Verma <abhi11verma@gmail.com>
2020-07-27 16:46:41 +0530

    ADD : App layout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="graphDataWrapper">
+        <div id="graphData"></div>
+        <div id="paths"></div>
+      </div>
+      <div id="graph"></div>
     </div>
   );
 }
