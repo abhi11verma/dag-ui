bb1cc33b3cbe1b5e600fb4c82c84e9c19b9b26b6
Abhishek Verma <abhi11verma@gmail.com>
2020-09-11 16:19:25 +0530

    Add build to host on github

---
+++ build/precache-manifest.00c480eb3354d7dde34d39fad12bd462.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c22a260344d5c9eee9b9ea200b6b96d8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f569333137773287190",
+    "url": "/static/css/main.56d28731.chunk.css"
+  },
+  {
+    "revision": "fa0f284d99dc007d397a",
+    "url": "/static/js/2.bc79ba81.chunk.js"
+  },
+  {
+    "revision": "0451d8bd08e7fcf6eb92b052953887b4",
+    "url": "/static/js/2.bc79ba81.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f569333137773287190",
+    "url": "/static/js/main.c303120c.chunk.js"
+  },
+  {
+    "revision": "2a229c976270be5b2670",
+    "url": "/static/js/runtime-main.4e7ff846.js"
+  }
+]);
